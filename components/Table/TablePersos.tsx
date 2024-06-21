2bbc04ecda8fd8df9b5950db0a2d9cf031e241f3
Amurius <artuslr78@gmail.com>
2024-06-21 22:15:24 +0200

    deployementReady

--- components/Table/TablePersos.tsx
+++ components/Table/TablePersos.tsx
@@ -5,7 +5,7 @@ import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, 
 import { useAsyncList } from "@react-stately/data";
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/react";
 import { DeleteIcon } from "./DeleteIcon";
-import {useGetNiveau} from "@/functions/getNiveau";
+import { useGetNiveau } from "@/functions/getNiveau";
 
 const statusColorMap: Record<string, ChipProps["color"]> = {
   active: "success",
@@ -16,6 +16,10 @@ const statusColorMap: Record<string, ChipProps["color"]> = {
 export default function TablePersos({ columns, data }: {
   columns: any, data: any
 }) {
+  const GetNiveau = (xp: any) => {
+    var niveau = useGetNiveau({ xpPerso: xp })
+    return niveau
+  }
   type Personnage = typeof data[0];
   const renderCell = React.useCallback((user: Personnage, columnKey: React.Key) => {
     const cellValue = user[columnKey as keyof Personnage];
@@ -33,66 +37,66 @@ export default function TablePersos({ columns, data }: {
           </div>
         );
       case "per_xp":
-        var niveau = useGetNiveau({ "xpPerso": cellValue });
+        var niveau = GetNiveau(cellValue);
         var xpRestant = niveau.xpRestant
         var xpSuivant = niveau.xpNivSuivant
         var xp = "";
-        var xpPerso ="";
-        if (xpSuivant < 1000){
+        var xpPerso = "";
+        if (xpSuivant < 1000) {
         }
-        else if (xpSuivant < (10**6)){
-          xp = Math.trunc(xpSuivant)/100 + "K"
+        else if (xpSuivant < (10 ** 6)) {
+          xp = Math.trunc(xpSuivant) / 100 + "K"
         }
-        else if (xpSuivant < (10**9)){
-          xp = Math.trunc(xpSuivant/1000)/1000 + "M"
+        else if (xpSuivant < (10 ** 9)) {
+          xp = Math.trunc(xpSuivant / 1000) / 1000 + "M"
         }
-        else if (xpSuivant < (10**12)){
-          xp = Math.trunc(xpSuivant/(10**6))/1000 + "B"
+        else if (xpSuivant < (10 ** 12)) {
+          xp = Math.trunc(xpSuivant / (10 ** 6)) / 1000 + "B"
         }
-        else if (xpSuivant < (10**15)){
-          xp = Math.trunc(xpSuivant/(10**9))/1000 + "T"
+        else if (xpSuivant < (10 ** 15)) {
+          xp = Math.trunc(xpSuivant / (10 ** 9)) / 1000 + "T"
         }
-        else if (xpSuivant < (10**18)){
-          xp = Math.trunc(xpSuivant/(10**12))/1000 + "Quad"
+        else if (xpSuivant < (10 ** 18)) {
+          xp = Math.trunc(xpSuivant / (10 ** 12)) / 1000 + "Quad"
         }
-        else if (xpSuivant < (10**21)){
-          xp = Math.trunc(xpSuivant/(10**15))/1000 + "Quint"
+        else if (xpSuivant < (10 ** 21)) {
+          xp = Math.trunc(xpSuivant / (10 ** 15)) / 1000 + "Quint"
         }
-        else if (xpSuivant < (10**24)){
-          xp = Math.trunc(xpSuivant/(10**18))/1000 + "SEX"
+        else if (xpSuivant < (10 ** 24)) {
+          xp = Math.trunc(xpSuivant / (10 ** 18)) / 1000 + "SEX"
         }
-        else{
-          xp = Math.trunc(xpSuivant/(10**21))/1000 + "GOD"
+        else {
+          xp = Math.trunc(xpSuivant / (10 ** 21)) / 1000 + "GOD"
         }
-        if (xpRestant < 1000){
+        if (xpRestant < 1000) {
         }
-        else if (xpRestant < (10**6)){
-          xpPerso = Math.trunc(xpRestant)/1000 + "K"
+        else if (xpRestant < (10 ** 6)) {
+          xpPerso = Math.trunc(xpRestant) / 1000 + "K"
         }
-        else if (xpRestant < (10**9)){
-          xpPerso = Math.trunc(xpRestant/1000)/1000 + "M"
+        else if (xpRestant < (10 ** 9)) {
+          xpPerso = Math.trunc(xpRestant / 1000) / 1000 + "M"
         }
-        else if (xpRestant < (10**12)){
-          xpPerso = Math.trunc(xpRestant/(10**6))/1000 + "B"
+        else if (xpRestant < (10 ** 12)) {
+          xpPerso = Math.trunc(xpRestant / (10 ** 6)) / 1000 + "B"
         }
-        else if (xpRestant < (10**15)){
-          xpPerso = Math.trunc(xpRestant/(10**9))/1000 + "T"
+        else if (xpRestant < (10 ** 15)) {
+          xpPerso = Math.trunc(xpRestant / (10 ** 9)) / 1000 + "T"
         }
-        else if (xpRestant < (10**18)){
-          xpPerso = Math.trunc(xpRestant/(10**12))/1000 + "Quad"
+        else if (xpRestant < (10 ** 18)) {
+          xpPerso = Math.trunc(xpRestant / (10 ** 12)) / 1000 + "Quad"
         }
-        else if (xpRestant < (10**21)){
-          xpPerso = Math.trunc(xpRestant/(10**15))/1000 + "Quint"
+        else if (xpRestant < (10 ** 21)) {
+          xpPerso = Math.trunc(xpRestant / (10 ** 15)) / 1000 + "Quint"
         }
-        else if (xpRestant < (10**24)){
-          xpPerso = Math.trunc(xpRestant/(10**18))/1000 + "SEX"
+        else if (xpRestant < (10 ** 24)) {
+          xpPerso = Math.trunc(xpRestant / (10 ** 18)) / 1000 + "SEX"
         } else {
-          xpPerso = Math.trunc(xpRestant/(10**21))/1000 + "GOD"
+          xpPerso = Math.trunc(xpRestant / (10 ** 21)) / 1000 + "GOD"
         }
         return (
           <>
             <div className="capitalize flex justify-between" >
-              <p className="lg:text-2xl text-sm capitalize ">{"Niv. "+niveau.niveau}</p>
+              <p className="lg:text-2xl text-sm capitalize ">{"Niv. " + niveau.niveau}</p>
               <p className="lg:text-2xl text-sm capitalize ">{xpPerso + " / " + xp}</p>
             </div>
           </>
@@ -119,7 +123,7 @@ export default function TablePersos({ columns, data }: {
         return cellValue;
     }
   }, []);
-  
+
   if (data) {
     return (
       <>
